78b939e6fb810f29d041c6513a092aca6f3ba3b4
Niko Dixon <nikodixon@gmail.com>
2020-06-13 09:03:12 -0500

    margins for street element adequate

--- hydro_gp_frontend/src/components/ConvertToPdf.js
+++ hydro_gp_frontend/src/components/ConvertToPdf.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
         fontFamily: "Times-Roman",
         fontSize: 10
     },
-    container: {
+    headerContainer: {
         display: "flex",
         borderWidth: 1, 
         padding: 1,
@@ -23,27 +23,41 @@ const styles = StyleSheet.create({
     },
     fieldTags: {
         fontFamily: "Times-Roman",
-        fontSize: 10
+        fontSize: 10,
+        height: "15px"
+        // borderWidth: 1,
+        // borderTop: 0,
+        // borderRight: 0,
+        // borderLeft: 0 
     },
     section1Main: {
         display: "flex",
         flexDirection: "row",
         padding: 1,
-        width: "90%",
+        width: "500px",
         marginLeft: "50px"
     },
     sec1Col1: {
-        width: "45%",
         borderWidth: 1,
         display: "flex",
+        flexDirection: "column",
         flex: 1
     }, 
     sec1Col2: {
-        width: "45%",
         borderWidth: 1,
         display: "flex",
+        flexDirection: "column",
         flex: 2,
         marginLeft: -1
+    },
+    street: {
+        display: "flex",
+        borderWidth: 1,
+        width: "498px",
+        marginLeft: "51px",
+        padding: 1,
+        marginTop: -1,
+        borderTop: 0 
     }
 })
 
@@ -65,7 +79,7 @@ export default class ConvertToPdf extends React.Component {
         return (
             <Document>
                 <Page size="A4" style={styles.page}>
-                    <View style={styles.container}>
+                    <View style={styles.headerContainer}>
                         <Text style={[styles.heading, {fontFamily: "Times-Bold"}]}>ENTRY FORM</Text>
                         <Text style={styles.heading}>EUROPCAR UKRAINIAN GRAND PRIX</Text>
                         <Text style={styles.heading}>Entry Forms returned to Race Secretary according to 4.4</Text>
@@ -73,11 +87,14 @@ export default class ConvertToPdf extends React.Component {
                         <Text style={[{fontSize:10}, {fontFamily: "Times-Bold"}, {marginLeft: "50px"}]}>Section 1</Text>
                     <View style={styles.section1Main}> 
                         <View style={styles.sec1Col1}>
-                            <Text style={styles.fieldTags}>First Name</Text>
+                            <Text style={styles.fieldTags}>First Name:</Text> 
                         </View>
                         <View style={styles.sec1Col2}>
-                            <Text style={styles.fieldTags}>Last Name</Text>
-                        </View>
+                            <Text style={styles.fieldTags}>Last Name:</Text>
+                        </View>   
+                    </View>
+                    <View style={styles.street}>
+                        <Text style={styles.fieldTags}>Street:</Text>
                     </View>
                 </Page>
             </Document>
