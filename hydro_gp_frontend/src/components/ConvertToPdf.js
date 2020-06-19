013edf59183b51f8b113081680de74553e319581
Niko Dixon <nikodixon@gmail.com>
2020-06-19 18:12:05 -0500

    Finishing touches on PDF template as well. Started dynamic data fields.

--- hydro_gp_frontend/src/components/ConvertToPdf.js
+++ hydro_gp_frontend/src/components/ConvertToPdf.js
@@ -14,8 +14,7 @@ const styles = StyleSheet.create({
         fontSize: 10
     },
     headerContainer: {
-        display: "flex",
-        borderWidth: 1, 
+        display: "flex", 
         padding: 1,
         textAlign: "center",
         width: "50%",
@@ -24,31 +23,35 @@ const styles = StyleSheet.create({
     fieldTags: {
         fontFamily: "Times-Roman",
         fontSize: 10,
-        height: "15px"
+        height: "20px",
         // borderWidth: 1,
         // borderTop: 0,
         // borderRight: 0,
         // borderLeft: 0 
     },
-    section1Main: {
+    namesMain: {
         display: "flex",
         flexDirection: "row",
         padding: 1,
+        paddingBottom: -1,
         width: "500px",
-        marginLeft: "50px"
+        marginLeft: "50px",
+        borderTop: 0
     },
     sec1Col1: {
         borderWidth: 1,
         display: "flex",
         flexDirection: "column",
-        flex: 1
+        flex: 1, 
+        padding: 1,
     }, 
     sec1Col2: {
         borderWidth: 1,
         display: "flex",
         flexDirection: "column",
         flex: 2,
-        marginLeft: -1
+        marginLeft: -1, 
+        padding: 1
     },
     street: {
         display: "flex",
@@ -57,8 +60,54 @@ const styles = StyleSheet.create({
         marginLeft: "51px",
         padding: 1,
         marginTop: -1,
-        borderTop: 0 
+        marginBottom: -1,
+        borderTop: 0,
+        borderBottom: 0
+    },
+    table: {
+        display: "table",
+        width: "498px",
+        marginLeft: "51px",
+        borderWidth: 1,
+        borderLeftWidth: 0,
+        borderRightWidth: 0,
+        borderTopWidth: 0
+    },
+    cell: {
+        display: "table-cell",
+        fontFamily: "Times-Roman",
+        height: "20px",
+        marginLeft: "2px",
+        fontSize: 10
+    },
+    tableRow: {
+        display: "flex",
+        flexDirection: "row",
+        borderWidth: 1,
+        borderRightWidth: 0,
+        borderBottomWidth: 0
+    },
+    tableColumn1: {
+        display: "flex",
+        flexDirection: "column",
+        flex: 1,
+        borderWidth: 1, 
+        borderLeftWidth: 0, 
+        borderTopWidth: 0,
+        borderBottomWidth: 0,
+        marginTop: -1 
+    },
+    tableColumn2: {
+        display: "flex",
+        flexDirection: "column",
+        flex: 2,
+        borderWidth: 1, 
+        borderLeftWidth: 0, 
+        borderTopWidth: 0,
+        borderBottom: 0,
+        marginTop: -1
     }
+
 })
 
 
@@ -82,20 +131,86 @@ export default class ConvertToPdf extends React.Component {
                     <View style={styles.headerContainer}>
                         <Text style={[styles.heading, {fontFamily: "Times-Bold"}]}>ENTRY FORM</Text>
                         <Text style={styles.heading}>EUROPCAR UKRAINIAN GRAND PRIX</Text>
+                        <Text style={styles.heading}> 26-27 July, 2019, Ternopil, Ukraine</Text>
                         <Text style={styles.heading}>Entry Forms returned to Race Secretary according to 4.4</Text>
                     </View>
                         <Text style={[{fontSize:10}, {fontFamily: "Times-Bold"}, {marginLeft: "50px"}]}>Section 1</Text>
-                    <View style={styles.section1Main}> 
+                    <View style={styles.namesMain}> 
                         <View style={styles.sec1Col1}>
-                            <Text style={styles.fieldTags}>First Name:</Text> 
+                            <Text style={styles.fieldTags}>Full Name:</Text> 
                         </View>
                         <View style={styles.sec1Col2}>
-                            <Text style={styles.fieldTags}>Last Name:</Text>
+                            <Text style={styles.fieldTags}>Date of Birth:</Text>
                         </View>   
                     </View>
                     <View style={styles.street}>
                         <Text style={styles.fieldTags}>Street:</Text>
                     </View>
+                    <View style={styles.table}>
+                        <View style={styles.tableRow}>
+                            <View style={styles.tableColumn1}>
+                                <Text style={styles.cell}>City:</Text>                         
+                            </View>
+                            <View style={styles.tableColumn2}>
+                                <Text style={styles.cell}>Zip: </Text>
+                            </View>
+                        </View>
+                        <View style={styles.tableRow}>
+                            <View style={styles.tableColumn1}>
+                                <Text style={styles.cell}>Country:</Text>                         
+                            </View>
+                            <View style={styles.tableColumn2}>
+                                <Text style={styles.cell}>Nationality:</Text>
+                            </View>
+                        </View>
+                        <View style={styles.tableRow}>
+                            <View style={styles.tableColumn1}>
+                                <Text style={styles.cell}>Mobile Phone:</Text>                         
+                            </View>
+                            <View style={styles.tableColumn2}>
+                                <Text style={styles.cell}>Email:</Text>
+                            </View>
+                        </View>
+                        <View style={[styles.tableRow, {borderRightWidth: 1}]}>
+                            <Text style={[{fontFamily: "Times-Bold"}, {fontSize: 10}, {height: "12px"}]}>Emergency Contact Information</Text>
+                        </View>
+                        <View style={styles.tableRow}>
+                            <View style={styles.tableColumn1}>
+                                <Text style={styles.cell}>Full Name:</Text>                         
+                            </View>
+                            <View style={styles.tableColumn2}>
+                                <Text style={styles.cell}>Relationship: </Text>
+                            </View>
+                        </View>
+                        <View style={[styles.tableRow, {borderRightWidth: 1}]}>
+                            <Text style={styles.cell}>Street</Text>
+                        </View>
+                        <View style={styles.tableRow}>
+                            <View style={styles.tableColumn1}>
+                                <Text style={styles.cell}>City:</Text>                         
+                            </View>
+                            <View style={styles.tableColumn2}>
+                                <Text style={styles.cell}>Zip: </Text>
+                            </View>
+                        </View>
+                        <View style={styles.tableRow}>
+                            <View style={styles.tableColumn1}>
+                                <Text style={styles.cell}>Country:</Text>                         
+                            </View>
+                            <View style={styles.tableColumn2}>
+                                <Text style={styles.cell}>Nationality:</Text>
+                            </View>
+                        </View>
+                        <View style={styles.tableRow}>
+                            <View style={styles.tableColumn1}>
+                                <Text style={styles.cell}>Mobile Phone:</Text>                         
+                            </View>
+                            <View style={styles.tableColumn2}>
+                                <Text style={styles.cell}>Email:</Text>
+                            </View>
+                        </View>
+                    </View>   
+                      
                 </Page>
             </Document>
         )
